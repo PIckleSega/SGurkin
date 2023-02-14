9e7592df051a067c23d9828c373d27fd72fb6a50
PIckleSega <92603787+PIckleSega@users.noreply.github.com>
2023-02-14 14:58:45 +0300

    Update Пр2.py

--- Пр2.py
+++ Пр2.py
@@ -1,3 +1,27 @@
-3
 
 
+print(ab)
+print(tseloe)
+print(drobnoe)
+print(tseloe)
+print(big_netseloe)
+print(drobnoe)
+print(с)
+print(stroka)
+
+a=1
+b=3
+print((a>b)and (b<a))
+print((b>a)and (b<a))
+print ((a>b)and(b>a))
+print(not(b>a))
+c = a+b
+print((c>b)or (b>a))
+print((a>c)or (b>c))
+print ((c<b)or(b<a))
+print((a<c) or (b>a))
+e ='Nasos'
+w = 'Nos'
+print((e>w) and (w>e))
+print((w>e)or (w < e))
+print(not(e<w))
