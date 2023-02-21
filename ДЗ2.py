a4f27211b8f3d423ea404118c17810df1c205bca
PIckleSega <92603787+PIckleSega@users.noreply.github.com>
2023-02-21 14:20:08 +0300

    Add files via upload

---
+++ ДЗ2.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+if a > b:
+    c = a - b
+else:
+    c = a + b
+if a == b:
+    c = a
+print(c)
+
+a = int(input())
+if a < 0:
+    print('SFG')
+elif a >= 2:
+    print('No')
+elif a = 1:
+    print('Yes')
+else:
+    print('Lets go')
